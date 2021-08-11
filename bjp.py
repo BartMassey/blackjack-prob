9bd589117e9be71073e732d0d59e36f086da3ffb
Bart Massey <bart.massey@gmail.com>
2021-08-11 13:42:53 -0700

    added comments

--- bjp.py
+++ bjp.py
@@ -1,41 +1,83 @@
+# Compute exact probabilities for achieving a Blackjack in 2..5 cards.
+# Bart Massey 2021
+
 from math import factorial, comb
 
+# A basic deck.
 deck = set(range(52))
 
+# Blackjack card value, which depends only on rank. Note
+# that this is nondeterministic, since aces can be played as
+# either 1 or 11.
 def value(card):
     rank = card % 13
+    # Spot card.
     if rank >= 2 and rank <= 9:
         return {rank}
+    # Ace.
     if rank == 0:
         return {1, 11}
+    # Face card.
     return {10}
 
+# For each number of already-drawn cards, compute the number
+# of remaining draws (assuming a 5-card draw) "controlled"
+# by this count. This allows early termination on blackjack
+# or bustout while still keeping the count correct.
 derate = [ factorial(i) * comb(52 - (5 - i), i) for i in range(0, 6) ]
 
-nleaves = 0
 
+# Counts the number of draws at which a position terminated,
+# accumulated over all runs of prob21(). This gives an
+# estimate of the pruning speedup.
+ndraws = 0
+
+# Given a current deck of undrawn cards, a set of cur possible
+# total scores, and the number of cards remaining to be drawn
+# for this probability, return the number of winning hands
+# and the number of hands considered.
+#
+# This statement is a bit confusing: see the code for details.
 def prob21(deck, cur, ncards):
-    global nleaves
+    global ndraws
+
+    # We have reached the target number of draws.
+    # Return whether or not we just reached a blackjack,
+    # and count 1.
     if ncards == 0:
-        nleaves += 1
+        ndraws += 1
         if 21 in cur:
             return 1, 1
         else:
             return 0, 1
+
+    # We have busted out, or hit blackjack early.
+    # Return zero, and count the number of hands
+    # that would have been evaluated if we had
+    # continued to the root.
     if not cur or 21 in cur:
-        nleaves += 1
+        ndraws += 1
         return 0, derate[ncards]
+
+    # We are still playing. Accumulate wins and counts from
+    # all possible successive positions.
     nw, nt = 0, 0
     for c in deck:
         v = value(c)
+        # Update the possible scores with all non-busted possibilities
+        # given the value of the current drawn card.
         next_cur = { v1 + s for v1 in v for s in cur if v1 + s <= 21 }
+        # Remove the drawn card from the deck.
         next_deck = deck - {c}
+        # Continue the calculation.
         w, t = prob21(next_deck, next_cur, ncards - 1)
+        # Update the totals
         nw += w
         nt += t
+    # Return accumulated counts.
     return nw, nt
 
 for i in range(2, 6):
     w, t = prob21(deck, {0}, i)
     print(i, w, t, 100 * w / t)
-print(nleaves)
+print(ndraws)
--- bjs.py
+++ bjs.py
@@ -1,32 +1,56 @@
+# Estimate probabilities for achieving a Blackjack in 2..5
+# cards by simulating games.
+# Bart Massey 2021
+
+# Number of simulation runs to perform.
 nsim = 1000000
 
 import random
 
+# A basic deck.
 deck = list(range(52))
 
+# Blackjack card value, which depends only on rank. Note
+# that this is nondeterministic, since aces can be played as
+# either 1 or 11.
 def value(card):
     rank = card % 13
+    # Spot card.
     if rank >= 2 and rank <= 9:
         return {rank}
+    # Ace.
     if rank == 0:
         return {1, 11}
+    # Face card.
     return {10}
 
+# Total number of blackjacks so far, indexed by number of
+# cards to get them.
 bj_cards = [0] * 6
 
+# Simulate a single game.
 def game():
     global deck
     random.shuffle(deck)
+    # Keep the running score for the game as a set of
+    # possibilities, based on the possible values of any
+    # aces encountered.
     score = {0}
+    # Draw up to five cards.
     for i, c in enumerate(deck[:5]):
         val = value(c)
         score = {s + v for s in score for v in val if s + v <= 21}
+
+        # Hit a blackjack, so done.
         if 21 in score:
             bj_cards[i] += 1
             return
+
+        # Busted, so done.
         if not score:
             return
 
+# Run out all the simulations.
 for _ in range(nsim):
     game()
 
